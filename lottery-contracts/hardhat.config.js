9375c25e9755fa88b57cc8ac25942f5536a7803c
Will Spencer <willspencer805@gmail.com>
2022-09-27 15:09:29 -0400

    updated config file

--- lottery-contracts/hardhat.config.js
+++ lottery-contracts/hardhat.config.js
@@ -1,4 +1,72 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_KEY || "key";
+
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      saveDeployments: true,
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    token: "MATIC",
+    noColors: true,
+    gasPriceApi:
+      "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["all"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    attendee1: {
+      default: 1,
+    },
+    attendee2: {
+      default: 2,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 500000, // 500 seconds max for running tests
+  },
 };
