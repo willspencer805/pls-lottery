e622ca4fd83b3fa66f29a1f460068095f3499180
Will Spencer <willspencer805@gmail.com>
2022-09-27 21:02:47 -0400

    added new tests for both contracts

--- lottery-contracts/test/Lottery.test.js
+++ lottery-contracts/test/Lottery.test.js
@@ -5,27 +5,25 @@ const {
   networkConfig,
 } = require("../helper-hardhat-config");
 
-describe("PLS Unit Tests", function () {
+describe("LotteryToken Unit Tests", function () {
   beforeEach(async function () {
     deployer = (await getNamedAccounts()).deployer;
     account1 = (await getNamedAccounts()).account1;
     account2 = (await getNamedAccounts()).account2;
-    await deployments.fixture(["pls"]);
+    await deployments.fixture(["all"]);
     pls = await ethers.getContract("PLS", deployer);
+    lottery = await ethers.getContract("LotteryToken", deployer);
     tokensPerEth = await pls.tokensPerEth();
   });
 
-  describe("constructor", function () {});
-
   describe("token purchase", function () {
-    it("mints the right amount of tokens", async function () {
-      const balanceBefore = await pls.balanceOf(deployer);
-      assert.equal(balanceBefore.toString(), "0");
-      const amount = 1;
-      await pls.buyTokens({ value: amount });
-      const amountToReceive = tokensPerEth * amount;
-      const balanceAfter = await pls.balanceOf(deployer);
-      assert.equal(balanceAfter.toString(), amountToReceive.toString());
+    it("transfer the right amount of tokens", async function () {
+      const amount = ethers.utils.parseEther("1000000000");
+      await pls.buyTokens({ value: 1 });
+      await pls.approve(lottery.address, amount);
+      await lottery.buyTickets(10);
+      balanceAfter = await lottery.balanceOf(deployer);
+      assert.equal(balanceAfter.toString(), "10");
     });
   });
 });
