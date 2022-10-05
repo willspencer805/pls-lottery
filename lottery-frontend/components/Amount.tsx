4f0fcbe8746878d740306d6e9e2ec14c6c42daaa
Will Spencer <willspencer805@gmail.com>
2022-10-05 17:27:05 -0400

    contract calls

--- lottery-frontend/components/Amount.tsx
+++ lottery-frontend/components/Amount.tsx
@@ -1,8 +1,24 @@
-async function buyTokens() {
-  console.log("boobs");
-}
+import { useState } from "react";
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import { abi } from "../utils/abi.js";
+import { ethers } from "ethers";
 
 export default function Amount() {
+  const [value, setValue] = useState("0");
+  const handleChange = (event: number) => {
+    setValue(event.target.value / 100);
+  };
+
+  const { config } = usePrepareContractWrite({
+    addressOrName: "0xF80B284285B8Fa0DE7E7808994184Aa2000fb874",
+    contractInterface: abi,
+    functionName: "buyTokens",
+    overrides: {
+      value: ethers.utils.parseEther(value.toString()),
+    },
+  });
+
+  const { data, isLoading, isSuccess, write } = useContractWrite(config);
   return (
     <div>
       <label
@@ -16,7 +32,8 @@ export default function Amount() {
           <span className="text-gray-500 sm:text-sm">Ξ</span>
         </div>
         <input
-          type="text"
+          type="number"
+          onChange={handleChange}
           name="price"
           id="price"
           className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
@@ -26,7 +43,8 @@ export default function Amount() {
       <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
         <div className="rounded-md shadow">
           <button
-            onClick={buyTokens}
+            disabled={!write}
+            onClick={() => write?.()}
             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
           >
             Get started
